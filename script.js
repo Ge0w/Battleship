b888e885e2fca252a3fe62d5a991557782abc1b8
Ge0h <george.walker.wme@gmail.com>
2022-05-08 17:49:49 +0100

    Ship factory function created

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const Ship = (length) => {
   const hits = [...Array(length).keys()].map((e) => (e = 0));
+  const hit = (index, ship) => {
+    hits[index] = 1;
+  };
   return {
     length: length,
     hits: hits,
     sunk: false,
+    hit,
   };
 };
 
-module.exports = { Ship };
+const isSunk = (ship) => {
+  if (ship.hits.every((e) => e === 1)) {
+    return (ship.sunk = true);
+  }
+};
+
+const Gameboard = () => {};
+
+module.exports = { Ship, isSunk };
--- script.test.js
+++ script.test.js
@@ -1,9 +1,18 @@
-import { Ship } from "./script";
+import { Ship, isSunk } from "./script";
 
 test("Creates ship of length 5", () => {
-  expect(Ship(5)).toStrictEqual({
-    length: 5,
-    hits: [0, 0, 0, 0, 0],
-    sunk: false,
-  });
+  expect(Ship(5).length).toBe(5);
+});
+
+test("Hits ship of length 5 at index 0", () => {
+  const ship = Ship(5);
+  ship.hit(0, ship);
+  expect(ship.hits).toStrictEqual([1, 0, 0, 0, 0]);
+});
+
+test("Checks ship of length 5 is sunk", () => {
+  const ship = Ship(5);
+  ship.hits = [1, 1, 1, 1, 1];
+  isSunk(ship);
+  expect(ship.sunk).toBe(true);
 });
