fff0c4c748be73b87785d61975206d13c4c3fb45
Ge0h <george.walker.wme@gmail.com>
2022-05-30 10:15:16 +0100

    Coordinates added to grid

--- src/dom-listener.js
+++ src/dom-listener.js
@@ -1,9 +1,26 @@
 // Create Gameboard function
 const createGameboard = (player) => {
+  //Create initial gameboard elements and append board to gameboard
   const gameboard = document.querySelector("#gameboard");
+  const boardContainer = document.createElement("div");
+  boardContainer.classList.add("container", "d-flex");
+  gameboard.append(boardContainer);
+  //Container to contain grid
   const gridContainer = document.createElement("div");
-  gameboard.append(gridContainer);
   gridContainer.classList.add("grid");
+  //Coordinates containers
+  const coordXContainer = document.createElement("div");
+  coordXContainer.classList.add("coord-x-container");
+  const coordYContainer = document.createElement("div");
+  coordYContainer.classList.add("coord-y-container");
+  //Container to wrap x-axis & grid
+  const gridAxis = document.createElement("div");
+  gridAxis.classList.add("d-flex", "flex-column");
+  //Finally append elements to board
+  boardContainer.append(coordYContainer);
+  boardContainer.append(gridAxis);
+  gridAxis.append(coordXContainer);
+  gridAxis.append(gridContainer);
 
   //Loop through gameboard array to create grid divs
   for (let i = 0; i < player.gameBoard.length; i++) {
@@ -11,23 +28,29 @@ const createGameboard = (player) => {
       const cell = document.createElement("div");
       cell.classList.add("cell");
       gridContainer.append(cell);
-      if (j < 1 && i < 9) {
-        const coordY = document.createElement("div");
-        coordY.classList.add("coord-y");
-        cell.append(coordY);
-        coordY.textContent = String.fromCharCode(49 + i);
-      } else if (j < 1 && i === 9) {
-        const coordY = document.createElement("div");
-        coordY.classList.add("coord-y");
-        cell.append(coordY);
-        coordY.textContent = 10;
-      }
-      if (i < 1) {
-        const coordX = document.createElement("div");
-        coordX.classList.add("coord-x");
-        cell.append(coordX);
-        coordX.textContent = String.fromCharCode(65 + j);
-      }
+    }
+  }
+
+  //Create coordinates
+
+  for (let i = 0; i < player.gameBoard.length; i++) {
+    const coordY = document.createElement("div");
+    coordY.classList.add("coord-y");
+    coordYContainer.append(coordY);
+    coordY.textContent = String.fromCharCode(65 + i);
+  }
+
+  for (let i = 0; i < player.gameBoard.length; i++) {
+    if (i === 9) {
+      const coordX = document.createElement("div");
+      coordX.classList.add("coord-x");
+      coordXContainer.append(coordX);
+      coordX.textContent = 10;
+    } else {
+      const coordX = document.createElement("div");
+      coordX.classList.add("coord-x");
+      coordXContainer.append(coordX);
+      coordX.textContent = String.fromCharCode(49 + i);
     }
   }
 };
